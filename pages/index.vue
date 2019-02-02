286c5fec77249e0ba46a6c6a9ffe76dcdabce20c
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2019-02-02 23:25:07 +0000

    Set your name

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <section class="section" />
+  <section class="section">
+    <label>Git name: </label>
+    <input v-model="name" class="input" type="text" placeholder="Name">
+    <a class="button is-primary" @click="changeName">
+      Set Name
+    </a>
+    <p>
+      Name: {{ name }}
+    </p>
+    <section />
+  </section>
 </template>
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    ...mapMutations(['SET_NAME']),
+    changeName() {
+      this.$store.commit('SET_NAME', this.name)
+    }
+  }
+}
+</script>
